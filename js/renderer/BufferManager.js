6941cdd09585fd963e671a3baad73c67a7c3eb77
Jordan-Casper <14353592+Jordan-Casper@users.noreply.github.com>
2023-12-04 01:08:01 -0700

    adjusted the createBatchBuffer() method

--- js/renderer/BufferManager.js
+++ js/renderer/BufferManager.js
@@ -14,24 +14,24 @@ class BufferManager {
         };
     }
 
-    createBatchBuffer(meshHandler, vertexBufferSize, indexBufferSize, normalBufferSize, texCoordBufferSize) {
+    createBatchBuffer(meshHandler) {
         this.batchBuffer.meshHandler = meshHandler;
         
         this.batchBuffer.vertexBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.batchBuffer.vertexBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertexBufferSize), this.gl.STATIC_DRAW);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.batchBuffer.meshHandler.vertexArray.length * Float32Array.BYTES_PER_ELEMENT, this.gl.STATIC_DRAW);
         
         this.batchBuffer.indexBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.batchBuffer.indexBuffer);
-        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(indexBufferSize), this.gl.STATIC_DRAW);
+        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, this.batchBuffer.meshHandler.indexArray.length * Uint32Array.BYTES_PER_ELEMENT, this.gl.STATIC_DRAW);
         
         this.batchBuffer.normalBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.batchBuffer.normalBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(normalBufferSize), this.gl.STATIC_DRAW);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.batchBuffer.meshHandler.normalArray.length * Float32Array.BYTES_PER_ELEMENT, this.gl.STATIC_DRAW);
         
         this.batchBuffer.texCoordBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.batchBuffer.texCoordBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(texCoordBufferSize), this.gl.STATIC_DRAW);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.batchBuffer.meshHandler.texCoordArray.length * Float32Array.BYTES_PER_ELEMENT, this.gl.STATIC_DRAW);
         
         return this.batchBuffer;
     }
